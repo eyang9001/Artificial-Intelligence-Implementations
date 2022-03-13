14e2427c070e3c6699bf0c7b96ae51916c473584
tergelm <40399394+tergelm@users.noreply.github.com>
2022-03-12 23:51:30 -0600

    Updated implementation with comments

--- 5_Bayes Net/student_code.py
+++ 5_Bayes Net/student_code.py
@@ -1,45 +1,55 @@
-
 def ask(var, value, evidence, bn):
-    #finds all of the unknown and known variables
-    evidence2 = evidence.copy()
-    evidence2[var] = value
+    e_copy_og = evidence.copy()
 
-    evidence3 = evidence.copy()
-    evidence3[var] = not value
+    evidence[var] = not value
+    e_copy_og[var] = value
 
-    unknown = list(set(bn.variable_names) - set(evidence2.keys()))
+    # make list of known variables
+    known_vars = e_copy_og.keys()
 
-    numerator = askRecursive(evidence2, bn, unknown, bn.variable_names.copy())
-    denom = numerator + askRecursive(evidence3, bn, unknown, bn.variable_names.copy())
-    quotient = numerator/denom
+    # calculate P(H|E): P(E|H) * P(H)
+    h_given_e = recursive(bn.variable_names.copy(), e_copy_og, known_vars,  bn)
+    # calculate P(~H|E): P(E|~H) * P(~H)
+    not_h_given_e = recursive(bn.variable_names.copy(), evidence, known_vars, bn)
+    # calculate alpha: P(E|H) * P(H) + P(E|~H) * P(~H)
+    alpha = h_given_e + not_h_given_e
+    # return P(H|E)
+    return (h_given_e/alpha)
 
-    return quotient
+def recursive(varlist, evidence, known_vars, bn):
+    # base case check
+    if not len(varlist):
+        return 1
+    else:
+        # get next variable
+        var = varlist.pop(0)
+        
+        # if next variable is known
+        if var in known_vars:
+            # look up probability in CPT using probability function in Bayes Net
+            ind = bn.variable_names.index(var)
+            p = bn.variables[ind].probability(evidence[var], evidence)
 
-def askRecursive(evidence, bn, unknown, varlist):
-    curvar = varlist.pop(0)
-
-    if curvar in unknown:
-        #  if the current variable is unknown will have to sum the values
-        if len(varlist) > 0:
-            trueevidence = evidence.copy()
-            trueevidence[curvar] = True
-            falseevidence = evidence.copy()
-            falseevidence[curvar] = False
-
-            varindex = bn.variable_names.index(curvar)
-            trueprob = bn.variables[varindex].probability(True, trueevidence)
-            falseprob = bn.variables[varindex].probability(False, falseevidence)
-
-            return (trueprob * askRecursive(trueevidence, bn, unknown, varlist.copy())) + (falseprob * askRecursive(falseevidence, bn, unknown, varlist.copy()))
+            # recurse on rest of varlist
+            jp = p * recursive(varlist.copy(), evidence, known_vars, bn)
+            return jp
+        
+        # if next variable is unknown
         else:
-            return 1
-    else:
-        varindex = bn.variable_names.index(curvar)
-        prob = bn.variables[varindex].probability(evidence[curvar], evidence)
+            # find joint probabilities when unknown is True or False
+            e_true = evidence.copy()
+            e_true[var] = True
+            e_false = evidence.copy()
+            e_false[var] = False
+            
+            ind = bn.variable_names.index(var)
+            p_true = bn.variables[ind].probability(True, e_true)
+            p_false = bn.variables[ind].probability(False, e_false)
+            
+            jp_true = p_true * recursive(varlist.copy(), e_true, known_vars, bn)
+            jp_false = p_false * recursive(varlist.copy(), e_false, known_vars,bn)
 
-    if len(varlist) > 0:
-        return prob * askRecursive(evidence, bn, unknown, varlist.copy())
-    else:
-        return prob
-
+            # compute sum and recurse
+            return  (jp_true + jp_false)
+           
 
